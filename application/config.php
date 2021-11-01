5f2390bb30e2ad3e487d50d4a79bf4c652be1da5
Nilefest <33624926+Nilefest@users.noreply.github.com>
2021-11-01 19:36:15 +0200

    Update config.php

--- application/config.php
+++ application/config.php
@@ -20,21 +20,18 @@ $config = array(
 	'db_type'      =>      'pdo',
 	'db_hostname'  =>      'localhost',
 	'db_port'      =>      3306,
-	'db_username'  =>      'kievdati_root',
-	'db_password'  =>      'kievdati_root123',
-	'db_database'  =>      'kievdati_agencydb',
-	/*'db_username'  =>      'u0701138_root',/**/
-	/*'db_password'  =>      'nilefest321',/**/
-	/*'db_database'  =>      'u0701138_agency',/**/
+	'db_username'  =>      'root',
+	'db_password'  =>      'root',
+	'db_database'  =>      'dbtest',
     
 	// Mail
 	'mail_host'    =>      'smtp.gmail.com',
 	'mail_port'    =>      '587',
-	'mail_login'   =>      'nikitaleo777333@gmail.com',
-	'mail_password'=>      '777333ln',
+	'mail_login'   =>      'mail@gmail.com',
+	'mail_password'=>      'pass',
 	'mail_sender'  =>      'KDA Suport',
 	'mail_unsub_link'  =>  'http://agency.nlf.name/service',
-	'mail_admin'   =>      'Datingagencykiev@gmail.com', // Dovga.vita@gmail.com / nikitaleo777333@gmail.com / Datingagencykiev@gmail.com
+	'mail_admin'   =>      'mail@gmail.com',
     
     // Tag for session and cookie
 	'session_tag'  =>      'agency',
